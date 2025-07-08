7c7805c2dd531fc4c43ddebab96845989b240b53
vrnxx <seryakov78116@gmail.com>
2025-07-08 17:27:31 +0300

    configure authenticate app

--- app/authenticate/admin.py
+++ app/authenticate/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'is_active', 'is_superuser')
+    list_display_links = ('id', 'email')
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
